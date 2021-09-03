5281680b2759e096f1fcbcdc32c97d715e784d74
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2021-09-03 08:45:20 -0300

    iniciada configuração do multer

--- server.js
+++ server.js
@@ -7,4 +7,17 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 app.get('/teste', (req, res) => res.send("ok"))
+ 
+// interpretar o formulário que veio o arquivo de upload
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, './upload')
+    },     
+    filename: function(req, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+
 app.listen(8080, () => console.log("Executando..."))
